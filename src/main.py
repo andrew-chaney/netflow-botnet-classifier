3dd974ee58e9974f25f7c07c7d23ead736248c0b
Andrew Chaney <andrewchaney24@gmail.com>
2024-02-22 12:29:04 -0700

    lstm language model analysis on feature sequences implemented

--- src/main.py
+++ src/main.py
@@ -3,12 +3,30 @@ import os
 
 import numpy as np
 
+from models.lstm_language_model import LSTMLanguageModel
+
 
 def load_data(path: str) -> np.ndarray:
+    """
+    Loads the data from a CSV data file path to a Numpy array.
+
+    :param path: path to the data file
+
+    :returns: 2D Numpy array of strings, each row corresponding to a line in
+              the file
+    """
     return np.loadtxt(path, delimiter=',', dtype=str)
 
 
 def split_data(data: np.ndarray, split: float=0.8) -> (np.ndarray, np.ndarray):
+    """
+    Splits data into training and testing sets based on unique IPs.
+
+    :param data: Numpy array of data
+    :param split: percent of data to train on (default: 80%)
+
+    :returns: tuple of Numpy arrays (training, testing)
+    """
     # Find all unique IPs
     unique_ips = np.unique(data[:, 1])
     # Get the training size and IPs to use for training
@@ -26,36 +44,71 @@ def main():
     # Process command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--input-path",
+        "--benign-path",
         type=str,
         required=True,
-        help="Input path to the sorted and aggregated data file",
+        help="Input path to the sorted, benign data file",
+    )
+    parser.add_argument(
+        "--bot-path",
+        type=str,
+        required=True,
+        help="Input path to the sorted, bot data file",
+    )
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=100,
+        required=False,
+        help="Number of times to train on the data set",
     )
 
     FLAGS = parser.parse_args()
 
-    # Check the provided path
-    if not os.path.exists(FLAGS.input_path):
-        print("ERROR: path provided is invalid")
+    # Check the provided paths
+    if not os.path.exists(FLAGS.benign_path):
+        print("ERROR: benign file path provided is invalid")
         return
 
-    input_path = os.path.abspath(FLAGS.input_path)
+    if not os.path.exists(FLAGS.bot_path):
+        print("ERROR: bot file path provided is invalid")
+        return
+
+    benign_path = os.path.abspath(FLAGS.benign_path)
+    bot_path = os.path.abspath(FLAGS.bot_path)
 
     # Load the data
-    data = load_data(input_path)
+    benign_data = load_data(benign_path)
+    bot_data = load_data(bot_path)
 
     # Split the data into a training and testing set
-    train, test = split_data(data)
+    ben_train, ben_test = split_data(benign_data)
+    bot_train, bot_test = split_data(bot_data)
 
     # Discard the timestamps, source ips, and destination ips
-    train = np.delete(train, np.s_[:3], axis=1)
-    test = np.delete(test, np.s_[:3], axis=1)
+    ben_train = np.delete(ben_train, np.s_[:3], axis=1)
+    ben_test = np.delete(ben_test, np.s_[:3], axis=1)
+    bot_train = np.delete(bot_train, np.s_[:3], axis=1)
+    bot_test = np.delete(bot_test, np.s_[:3], axis=1)
 
     # Split off the labels from the features
-    train_label = train[:, 0]
-    train_feat = train[:, 1:]
-    test_label = test[:, 0]
-    test_feat = test[:, 1:]
+    ben_train_label = ben_train[:, 0]
+    ben_train_feat = ben_train[:, 1:]
+    ben_test_label = ben_test[:, 0]
+    ben_test_feat = ben_test[:, 1:]
+    bot_train_label = bot_train[:, 0]
+    bot_train_feat = bot_train[:, 1:]
+    bot_test_label = bot_test[:, 0]
+    bot_test_feat = bot_test[:, 1:]
+
+    # Set up the model and start training on benign training data.
+    model = LSTMLanguageModel(ben_train_feat, epochs=FLAGS.epochs)
+    model.train()
+    # Evaluate the predictions that the model makes
+    benign_log_prob = model.evaluate_predictions(ben_test_feat)
+    bot_log_prob = model.evaluate_predictions(bot_test_feat)
+    print(f"Avg. Log Probability for Benign Testing: {benign_log_prob}")
+    print(f"Avg. Log Probability for Bot Testing:    {bot_log_prob}")
 
 
 if __name__ == "__main__":
---
+++ src/models/lstm_language_model.py
@@ -0,0 +1,174 @@
+from tqdm import tqdm
+
+from keras import Input
+from keras.layers import Dense, Embedding, LSTM
+from keras.models import Sequential
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.utils import to_categorical
+import numpy as np
+
+
+class LSTMLanguageModel:
+    """
+    LSTM Model that works to predict the next feature from a set of netflow
+    data features.
+
+    :param features: 2D Numpy array of Netflow features data
+    :param epochs: Number of times for model to train on the provided dataset
+    """
+
+    def __init__(self, features: np.ndarray, epochs: int = 100):
+        # Persist training data
+        self.X = features
+        self.epochs = epochs
+        # Flags for defining LSTM state
+        self.data_ready = False
+        self.model_ready = False
+        self.model_trained = False
+
+    def train(self, verbose: bool = True) -> None:
+        """
+        Trains the model on the data, preparing the data and model if needed.
+
+        :param verbose: Display output as the model trains
+        """
+        if not self.data_ready or not self.model_ready:
+            self.__prep_training_data__()
+            self.__activate_layers__()
+
+        v = 1 if verbose else 0
+        self.model.fit(self.X, self.y, epochs=self.epochs, verbose=v)
+        self.model_trained = True
+
+    def evaluate(self, data: np.ndarray) -> None:
+        """
+        Run the model's evaluation function given some testing data.
+
+        :param data: 2D Numpy array of Netflow features data
+        """
+        test_X, test_y = self.__prep_testing_data__(data)
+        self.model.evaluate(test_X, test_y)
+
+    def evaluate_predictions(self, data: np.ndarray) -> np.float_:
+        """
+        Given some testing data, find the probability of accurate predictions
+        throughout a Netflow's sequence of features.
+
+        :param data: 2D Numpy array of Netflow features data
+
+        :returns: Numpy Float of the average log probability for the sequences
+                  based on the model's understanding
+        """
+        test_X, test_y = self.__prep_testing_data__(data)
+        probabilities = []
+        predictions = self.model.predict(test_X)
+        for y, probs in zip(test_y, predictions):
+            probabilities.append(np.log(probs[np.where(y == 1)[0][0]]))
+        return np.average(probabilities)
+
+    def __activate_layers__(self) -> None:
+        """
+        Set up the LSTM Model and all of its layers for testing.
+        """
+        if self.model_ready:
+            return
+
+        if not self.data_ready:
+            self.__prep_training_data__()
+
+        # Instantiate the model
+        self.model = Sequential()
+        # Layer 0 - Input Layer
+        self.model.add(Input(shape=(self.X.shape[1],)))
+        # Layer 1 - Embedding Layer
+        self.model.add(Embedding(self.total_words, 100))
+        # Layer 2 - LSTM Layer
+        self.model.add(LSTM(150, activation="tanh"))
+        # Layer 3 - SoftMax (Output) Layer
+        self.model.add(Dense(self.total_words, activation="softmax"))
+        self.model.compile(
+            optimizer="adam",
+            loss="categorical_crossentropy",
+            metrics=["accuracy"],
+        )
+        self.model_ready = True
+
+    def __tokenize__(self, data: np.ndarray) -> None:
+        """
+        Fit the Tokenizer to the data sequences.
+
+        :param data: Numpy array of data sequence in the form of strings
+        """
+        self.tokenizer = Tokenizer(filters='!"#$%&()*,/:;<=>?@[\\]^_`{|}~\t\n')
+        for seq in tqdm(data, desc="Fitting Tokenizer"):
+            self.tokenizer.fit_on_texts([seq])
+        self.total_words = len(self.tokenizer.word_index) + 1
+
+    def __sequence__(self, data: np.ndarray) -> None:
+        """
+        Generate padded, tokenized sequences to train on from the data. Saves
+        generated sequences and information to the object to be called later.
+
+        :param data: 1D Numpy array of Netflow feature data strings
+        """
+        self.sequences = []
+        # Generate tokenized sequences
+        for seq in tqdm(data, desc="Tokenizing Sequences"):
+            tokens = self.tokenizer.texts_to_sequences([seq])[0]
+            for i in range(1, len(tokens)):
+                self.sequences.append(tokens[:i+1])
+        # Pad the sequences to the same length
+        self.max_len = max([len(seq) for seq in self.sequences])
+        self.sequences = np.array(pad_sequences(
+            self.sequences, maxlen=self.max_len, padding="pre",
+        ))
+
+    def __flatten_to_strings__(
+            self, arr: np.ndarray, delim: str = ' ') -> np.ndarray:
+        """
+        Takes a 2D Numpy array, flattens each sub-array to a string joined by
+        a specified delimiter, and returns the resulting 1D array of strings.
+
+        :param arr: 2D Numpy array of data
+        :param delim: delimiter for separating strings
+
+        :returns: 1D Numpy array of strings
+        """
+        return np.fromiter(map(lambda x: delim.join(x), arr), dtype=object)
+
+    def __prep_training_data__(self) -> None:
+        """
+        Run the data preparation pipeline on the provided Netflow feature
+        data.
+        """
+        if self.data_ready:
+            return
+
+        self.X = self.__flatten_to_strings__(self.X)
+        self.__tokenize__(self.X)
+        self.__sequence__(self.X)
+        self.X = self.sequences[:, :-1]
+        self.y = np.array(to_categorical(
+            self.sequences[:, -1], num_classes=self.total_words,
+        ))
+        self.data_ready = True
+
+    def __prep_testing_data__(
+            self, data: np.ndarray) -> (np.ndarray, np.ndarray):
+        """
+        """
+        data = self.__flatten_to_strings__(data)
+        sequences = []
+        for d in data:
+            # Tokenize the data point and break into sequences to predict on
+            tokens = self.tokenizer.texts_to_sequences([d])[0]
+            sequences.extend([tokens[:i+1] for i in range(1, len(tokens))])
+        sequences = pad_sequences(
+            sequences, maxlen=self.max_len, padding="pre",
+        )
+        X = sequences[:, :-1]
+        y = np.array(to_categorical(
+            sequences[:, -1], num_classes=self.total_words,
+        ))
+        return (X, y)
